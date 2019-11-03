0049cab3d1f3054a6a33032e5df6b5d419d2cd21
Mike Brown <mike.brown@excella.com>
2019-11-03 14:51:15 -0500

    load yaml error handling

--- plz/config.py
+++ plz/config.py
@@ -1,32 +1,76 @@
 import yaml
 import sys
 import os
+import textwrap
 from colorama import Fore, Style
 from .runner import run_command
 import sh
 
+DOC_URL = "https://github.com/m3brown/plz"
+
+
+class NoFileException(Exception):
+    pass
+
+
+class InvalidYamlException(Exception):
+    def __init__(self, *args, **kwargs):
+        if len(args) == 1:
+            setattr(self, "filename", args[0])
+        return super().__init__(*args, **kwargs)
+
+
+def invalid_directory():
+    print(Fore.RED + textwrap.dedent("""
+        plz must be run from:
+          a) a directory that has a valid plz.yaml file or
+          b) within a git repo that contains a plz.yaml in the repo root path.
+
+        For more information, visit {}
+        """).format(DOC_URL) + Style.RESET_ALL)
+    sys.exit(1)
+
+
+def invalid_yaml(filename):
+    print(Fore.RED + textwrap.dedent("""
+        [ERROR] Error parsing yaml config: {}
+
+        For more information on .plz.yaml formatting, visit {}
+        """.format(filename, DOC_URL)) + Style.RESET_ALL)
+    sys.exit(1)
+
 
 def git_root():
-    output = sh.Command('git')('rev-parse', '--show-toplevel')
-    if output.exit_code == 0:
+    try:
+        output = sh.Command('git')('rev-parse', '--show-toplevel')
         return str(output).strip()
-    else:
-        print(Fore.RED + 'Could not find root of git repository' + Style.RESET_ALL)
-        sys.exit(1)
+    except sh.ErrorReturnCode_128:
+        raise NoFileException()
 
 
 def load_config(filename):
-    print(Fore.CYAN)
-    print("[INFO] Using config: {}".format(filename))
-    print(Style.RESET_ALL)
-    return yaml.load(open(filename), Loader=yaml.SafeLoader)
+    try:
+        config = yaml.load(open(filename), Loader=yaml.SafeLoader)
+        print(Fore.CYAN)
+        print("[INFO] Using config: {}".format(filename))
+        print(Style.RESET_ALL)
+        return config
+    except yaml.YAMLError as e:
+        raise InvalidYamlException(filename)
 
 
 def plz_config():
     filename = '.plz.yaml'
-    if os.path.isfile(filename):
-        return (load_config(filename), None)
-    else:
-        root = git_root().rstrip('/')
-        config_file = '{}/{}'.format(root, filename)
-        return (load_config(config_file), root)
+    try:
+        if os.path.isfile(filename):
+            return (load_config(filename), None)
+        else:
+            root = git_root().rstrip('/')
+            full_path = os.path.join(root, filename)
+            if os.path.isfile(full_path):
+                return (load_config(full_path), root)
+            raise NoFileException
+    except NoFileException:
+        invalid_directory()
+    except InvalidYamlException as e:
+        invalid_yaml(e.filename)
--- tests/config_test.py
+++ tests/config_test.py
@@ -1,6 +1,7 @@
-from plz.config import plz_config, load_config, git_root
+from plz.config import plz_config, load_config, git_root, NoFileException, InvalidYamlException
 from io import StringIO
 import pytest
+import sh
 import sys
 
 if sys.version_info.major > 2:
@@ -31,7 +32,7 @@ def test_plz_config_falls_back_to_git_root_file(mock_load_config, mock_git_root,
     # Arrange
     test_path = '/test/root/path'
     mock_git_root.return_value = test_path
-    mock_isfile.return_value = False
+    mock_isfile.side_effect = [False, True]
 
     # Act
     plz_config()
@@ -42,15 +43,32 @@ def test_plz_config_falls_back_to_git_root_file(mock_load_config, mock_git_root,
 
 @patch('os.path.isfile')
 @patch('plz.config.git_root')
-def test_plz_config_aborts_if_empty_git_root(mock_git_root, mock_isfile):
+@patch('plz.config.invalid_directory')
+def test_plz_config_aborts_if_empty_git_root(mock_invalid_directory, mock_git_root, mock_isfile):
     # Arrange
     mock_git_root.return_value = ''
     mock_isfile.return_value = False
 
     # Act
+    plz_config()
+
     # Assert
-    with pytest.raises(Exception):
-        plz_config()
+    mock_invalid_directory.assert_called_once()
+
+
+@patch('os.path.isfile')
+@patch('plz.config.load_config')
+@patch('plz.config.invalid_yaml')
+def test_plz_config_aborts_if_InvalidYamlException(mock_invalid_yaml, mock_load_config, mock_isfile):
+    # Arrange
+    mock_isfile.return_value = True
+    mock_load_config.side_effect = InvalidYamlException("filename")
+
+    # Act
+    plz_config()
+
+    # Assert
+    mock_invalid_yaml.assert_called_once()
 
 
 @patch('os.path.isfile')
@@ -73,7 +91,7 @@ def test_plz_config_handles_extra_trailing_slash(mock_load_config, mock_git_root
     # Arrange
     test_path = '/test/root/path'
     mock_git_root.return_value = test_path + "/"
-    mock_isfile.return_value = False
+    mock_isfile.side_effect = [False, True]
 
     # Act
     plz_config()
@@ -83,7 +101,7 @@ def test_plz_config_handles_extra_trailing_slash(mock_load_config, mock_git_root
 
 
 @patch('{}.open'.format(builtins_module))
-def test_laod_config_loads_yaml_file(mock_open):
+def test_load_config_loads_yaml_file(mock_open):
     # Arrange
     mock_open.return_value = StringIO(u"""- id: run
   name: runserver
@@ -102,20 +120,31 @@ def test_laod_config_loads_yaml_file(mock_open):
     assert(result == expected_result)
 
 
-@patch('sys.exit')
-@patch('sh.Command')
-def test_git_root_with_bad_rc(mock_sh, mock_exit):
+@patch('{}.open'.format(builtins_module))
+def test_load_config_aborts_if_bad_yaml_file(mock_open):
     # Arrange
-    mock_sh_git = MagicMock()
-    mock_sh_git.__repr__ = lambda x: '/sample/path'
-    type(mock_sh_git).exit_code = PropertyMock(return_value=10)
-    mock_sh().return_value = mock_sh_git
+    mock_open.return_value = StringIO(u"""- id: run
+  name: runserver
+  cmd: echo "./manage.py runserver"
+  foo
+""")
 
     # Act
-    git_root()
-
     # Assert
-    mock_exit.assert_called_with(1)
+    with pytest.raises(InvalidYamlException):
+        result = load_config('path')
+
+
+@patch('sys.exit')
+@patch('sh.Command')
+def test_git_root_with_git_128_exception_raises_NoFileException(mock_sh, mock_exit):
+    # Arrange
+    mock_sh().side_effect = sh.ErrorReturnCode_128(b"", b"", b"")
+
+    # Act
+    # Assert
+    with pytest.raises(NoFileException):
+        git_root()
 
 
 @patch('sh.Command')
